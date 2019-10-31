932da29897abf11b9d6a7422711b650dfe4add9b
jannis <jluebbe@uos.de>
2019-10-31 23:27:46 +0100

    utilising turf.js to generate aircraftPosition feature.

--- static/aircraft_static.js
+++ static/aircraft_static.js
@@ -78,30 +78,21 @@ function refreshAircraftPositions() {
                     return;
                 else if (aircraft[5] === null || aircraft[6] === null)
                     return;
-                var aircraftPosition = ({
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [aircraft[5], aircraft[6]]
-                    },
-                    "type": "Feature",
-                    "properties": {
-                        "callsign": callsign,
-                        "heading": Math.round(aircraft[10]),
-                        "ul_visible": "both",
-                        "flight_level": Math.round(aircraft[7] / 0.3048 / 100),
-                        "ground_speed": Math.round(aircraft[9] * (3600 / 1852)),
-                        "vertical_speed": Math.round(aircraft[11] * (60 / 0.3048))
-                    }
+                var aircraftPosition = turf.point([aircraft[5], aircraft[6]], {
+                    "callsign": callsign,
+                    "heading": Math.round(aircraft[10]),
+                    "ul_visible": "both",
+                    "flight_level": Math.round(aircraft[7] / 0.3048 / 100),
+                    "ground_speed": Math.round(aircraft[9] * (3600 / 1852)),
+                    "vertical_speed": Math.round(aircraft[11] * (60 / 0.3048))
                 });
                 for (var i = 0, len = upper_lower_airspace_limits.features.length; i < len; i++) {
                     let airspace = upper_lower_airspace_limits.features[i];
-                    if (aircraftPosition.properties.flight_level < airspace.properties.MIN_FLIGHT)
-                        continue;
-                    else if (aircraftPosition.properties.flight_level > airspace.properties.MAX_FLIGHT)
-                        continue;
-                    else if (turf.booleanPointInPolygon(aircraftPosition, airspace)) {
-                        aircraftPosition.properties.ul_visible = airspace.properties.UL_VISIBLE;
-                        break;
+                    if (aircraftPosition.properties.flight_level >= airspace.properties.MIN_FLIGHT &&
+                        aircraftPosition.properties.flight_level <= airspace.properties.MAX_FLIGHT &&
+                        turf.booleanPointInPolygon(aircraftPosition, airspace)) {
+                            aircraftPosition.properties.ul_visible = airspace.properties.UL_VISIBLE;
+                            break;
                     }
                 }
                 aircraftPositions.push(aircraftPosition);
