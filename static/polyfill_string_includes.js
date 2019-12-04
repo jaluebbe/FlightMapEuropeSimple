d1623d141c32ae519fb54da690663d21190bcf0c
jannis <jluebbe@uos.de>
2019-12-04 20:22:30 +0100

    added polyfill for string includes.

---
+++ static/polyfill_string_includes.js
@@ -0,0 +1,15 @@
+// taken from https://developer.mozilla.org
+if (!String.prototype.includes) {
+  String.prototype.includes = function(search, start) {
+    'use strict';
+    if (typeof start !== 'number') {
+      start = 0;
+    }
+    
+    if (start + search.length > this.length) {
+      return false;
+    } else {
+      return this.indexOf(search, start) !== -1;
+    }
+  };
+}
