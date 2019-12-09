06e40ddc43fdee150c1f53e62b23f61ee391cbed
jannis <jluebbe@uos.de>
2019-12-09 01:44:06 +0100

    added flightsearch to backend_fastapi.py including validation of input. Database queries are not implemented yet.

--- backend_fastapi.py
+++ backend_fastapi.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Query
 from starlette.staticfiles import StaticFiles
 from starlette.responses import FileResponse
+from pydantic import BaseModel, confloat, conint, constr
 import backend_vrs_db
 
 app = FastAPI(
@@ -9,11 +10,27 @@ app = FastAPI(
     description=''
     )
 
+class Location(BaseModel):
+    lat: confloat(ge=-90, le=90)
+    lng: confloat(ge=-180, le=180)
+
+class FlightSearch(BaseModel):
+    origin: Location
+    destination: Location
+    originRadius: conint(gt=0, le=600e3)
+    destinationRadius: conint(gt=0, le=600e3)
+    numberOfStops: conint(ge=0, le=2)
+    filterAirlineAlliance: constr(regex='^(Star Alliance|Oneworld|SkyTeam|)$')
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", include_in_schema=False)
 async def root():
     return FileResponse('static/flightmap_europe_simple.html')
+
+@app.get("/flightsearch.html", include_in_schema=False)
+async def root():
+    return FileResponse('static/flightsearch.html')
 
 @app.get("/api/geojson/airports")
 def get_geojson_airports():
@@ -35,3 +52,7 @@ def get_geojson_airline(
 def get_geojson_airport(
     icao: str = Query(..., min_length=4, max_length=4, regex="^[0-9A-Z]{4}$")):
     return backend_vrs_db.get_geojson_airport(icao)
+
+@app.post("/api/geojson/flightsearch")
+def post_geojson_flightsearch(data: FlightSearch):
+    return {'data': data}
