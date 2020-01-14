ee8ac27a5818ece520801834bde56c2e6553b063
jaluebbe <jluebbe@uos.de>
2020-01-14 20:12:08 +0100

    added string_to_colour.js
    This code creates a colour code from a string. It may by used to assign different colours to aircraft callsigns.

---
+++ static/string_to_colour.js
@@ -0,0 +1,14 @@
+// inspired by https://stackoverflow.com/questions/3426404/create-a-hexadecimal-colour-based-on-a-string-with-javascript
+String.prototype.getHashCode = function() {
+    var hash = 0;
+    if (this.length == 0) return hash;
+    for (var i = 0; i < this.length; i++) {
+        hash = this.charCodeAt(i) + ((hash << 5) - hash);
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+};
+Number.prototype.intToHSL = function() {
+    var shortened = this % 360;
+    return "hsl(" + shortened + ",100%,30%)";
+};
