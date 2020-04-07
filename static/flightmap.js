39ae2328888f4327b07fcfedf8dc7820e4697917
jannis <jluebbe@uos.de>
2020-04-07 21:45:01 +0200

    modified default map zoom level.

--- static/flightmap.js
+++ static/flightmap.js
@@ -17,7 +17,12 @@ function addPrivacyStatement() {
     xhr.send();
 }
 addPrivacyStatement();
-map.setView([52, 4.5], 6);
+if (L.Browser.mobile) {
+    map.setView([52, 4.5], 3.5);
+    map.removeControl(map.zoomControl);
+} else {
+    map.setView([52, 4.5], 4);
+}
 L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
     minZoom: 1,
     maxZoom: 15,
@@ -25,9 +30,6 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
         '&copy; <a href="https://carto.com/attributions">CARTO</a>',
 }).addTo(map);
-if (L.Browser.mobile) {
-    map.removeControl(map.zoomControl);
-}
 var layerControl = L.control.layers({}, {}, {
     collapsed: L.Browser.mobile, // hide on mobile devices
     position: 'topright',
