7c8d2dd2d1fc6c2f1e7de70516debbc56f38ba2b
jannis <jluebbe@uos.de>
2021-02-11 20:05:09 +0100

    modified method to select airports from ourairports.com .

--- prepare_static_airports.py
+++ prepare_static_airports.py
@@ -4,28 +4,34 @@ import csv
 import requests
 import json
 import re
+from collections import Counter
 import clip_geojson_precision
 
 CSV_URL = 'https://ourairports.com/data/airports.csv'
 
-ignored_icaos = ['OPDD', 'OP17', 'VO55', 'EK_4']
 accepted_icaos = []
 ignored_types = []
 # possible types: 'balloonport', 'heliport', 'seaplane_base', 'small_airport',
 # 'medium_airport', 'large_airport', ''
 
+icao_pattern = re.compile('^[A-Z]{4}$')
+
 with requests.Session() as s:
     download = s.get(CSV_URL)
-    print(download.encoding)
-    download.encoding = 'utf-8' 
+    download.encoding = 'utf-8'
 reader = csv.DictReader(download.text.splitlines(), delimiter=',')
+airports = [
+    row for row in reader if icao_pattern.match(row['gps_code']) is not None]
+
+icao_count = Counter()
+for row in airports:
+    icao_count.update([row['gps_code']])
+icao_count.subtract(list(icao_count))
+duplicate_icaos = set(+icao_count)
+
 _features = []
-for row in reader:
-    if row['ident'] in ignored_icaos:
-        continue
+for row in airports:
     if row['type'] in ignored_types:
-        continue
-    if re.match('^[A-Z]{4}$', row['gps_code']) is None:
         continue
     if not len(row['iata_code']) in (0, 3):
         if row['iata_code'] == '0':
@@ -37,6 +43,11 @@ for row in reader:
     if len(row['iata_code']) != 3 and not row['gps_code'] in accepted_icaos:
         continue
     if row['scheduled_service'] == 'no':
+        continue
+    if row['gps_code'] in duplicate_icaos and row['ident'] != row['gps_code']:
+        print(
+            f"ignoring duplicate entry {row['ident']} for "
+            f"{row['gps_code']} / {row['iata_code']}.")
         continue
     _point = {"type": "Point", "coordinates": [float(row['longitude_deg']),
         float(row['latitude_deg'])]}
@@ -51,6 +62,7 @@ _collection = {"type": "FeatureCollection", "properties": {},
     "features": _features}
 
 file_name = 'static/airports_static.json'
+print(f"writing {len(_features)} airports to {file_name}.")
 with open(file_name, 'w') as f:
     json.dump(_collection, f)
 
